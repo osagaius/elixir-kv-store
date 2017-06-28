69bbaf5524e88d21cda47cfc6e9d71bdb6e7a3f2
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-06-27 21:29:42 -0400

    add test for updating value for a given key

--- test/controllers/kv_controller_test.exs
+++ test/controllers/kv_controller_test.exs
@@ -33,4 +33,20 @@ defmodule ElixirKvStore.KVControllerTest do
     conn = get conn, "/api/get/#{key}"
     assert json_response(conn, 200) == val
   end
+
+  test "POST /api/add?key=key&value=value UPDATE KEY", %{conn: conn} do
+    key = "test_key2"
+    val = "test_val"
+
+    conn = post conn, "/api/add?key=#{key}&value=#{val}"
+    assert json_response(conn, 200)
+
+    new_val = "test_val2"
+
+    conn = post conn, "/api/add?key=#{key}&value=#{new_val}"
+    assert json_response(conn, 200)
+
+    conn = get conn, "/api/get/#{key}"
+    assert json_response(conn, 200) == new_val
+  end
 end
