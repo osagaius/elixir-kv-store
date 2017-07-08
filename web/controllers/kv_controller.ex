7644ac931f4881d93b906817def518ac5e629542
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-07-08 12:27:00 -0400

    add controller methods and routes for ttl

--- web/controllers/kv_controller.ex
+++ web/controllers/kv_controller.ex
@@ -1,6 +1,7 @@
 defmodule ElixirKvStore.KVController do
   use ElixirKvStore.Web, :controller
   alias ElixirKvStore.Store
+  require Logger
 
   def index(conn, _params) do
     keys = Store.keys()
@@ -9,6 +10,20 @@ defmodule ElixirKvStore.KVController do
 
   def get(conn, params=%{"key" => key}) do
     result = Store.fetch(key)
+    json conn, result
+  end
+
+  def get_ttl(conn, params=%{"key" => key}) do
+    result = Store.get_ttl(key)
+    json conn, result
+  end
+
+  def add(conn, params=%{"key" => key, "value" => value, "expiration" => exp}) do
+    result = case exp |> Integer.parse do
+      {number, _} -> Store.set(key, value, number)
+      _ -> Store.set(key, value)
+    end
+
     json conn, result
   end
 
